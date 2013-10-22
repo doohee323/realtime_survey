6e67808d81c613f05fa8aae3324d33a5da6c5391
doohee <doohee323@gmail.com>
2013-10-22 14:59:10 +0900

    reflected

--- rts_client/app/scripts/controllers/join.js
+++ rts_client/app/scripts/controllers/join.js
@@ -9,5 +9,67 @@
 'use strict';
 
 angular.module('rtsClientApp')
-  .controller('JoinCtrl', function ($scope) {
+  .controller('JoinCtrl', function ($location, $scope, $state, $http, UserService) {
+  	//alert('user->' + $scope.user.email + "pwd: ->" + $scope.user.password);
+    // Signup action
+
+    $scope.signup = function() {
+      $http({
+        url: '/api/users',
+        method: 'POST',
+        data: { user: $scope.user }
+      }).success(function(data) {
+        // If the request is successful, and the user
+        // has been created: store the auth_token,
+        // broadcast the 'authenticated' event across
+        // our app, and redirect to '/'
+        UserService.set( data.auth_token );
+        $scope.$broadcast('event:authenticated');
+        $location.path('/');
+      }).error(function(reason) {
+      	alert('fail');
+        // Store the errors to format on screen
+        $scope.user.errors = reason;
+      });
+    };
+
+    // logOut
+    $scope.logout = function() {
+      // alert('auth_token:' + UserService.get());
+    	// Post to our api sign_in route
+      $http({
+        url: '/api/users/sign_out',
+        method: 'DELETE',
+        data: {auth_token: UserService.get()}
+      }).success(function(result) {
+        $location.path('/');
+      }).error(function(result){
+        $scope.ngModel = data;
+        $scope.user.errors = data.info;
+     });
+  };
+
+    // login action
+    $scope.login = function() {
+      // Post to our api sign_in route
+      $http({
+        url: '/api/users/sign_in',
+        method: 'POST',
+        data: { user: $scope.user }
+      }).success(function(data) {
+        if (data.success) {
+          // If we get back an authenticated user (indicated by
+          // the success in the response, not if the response 
+          // returns 401 - so we can capture the user errors
+          $scope.ngModel = data.data.data;
+          UserService.set(data.data.auth_token);
+          $state.go('app.paperlist');
+
+        } else {
+          $scope.ngModel = data;
+          $scope.user.errors = data.info;
+        }
+      });
+    };
+
   });
